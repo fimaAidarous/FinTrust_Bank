9f3295de734bcc98688d5d2419f12279f7c6af70
fimaAidarous <fma0899@gmail.com>
2025-03-03 23:13:11 -0800

    getSingleUser

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { test, createUser } 
+import { test, createUser,
+    getUser
+ } 
 from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get('/test', test);
 router.post('/createUser', createUser);
+router.get("/:id", getUser);
 
 export default router;
