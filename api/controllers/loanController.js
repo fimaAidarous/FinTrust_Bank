8e6a431333e7f03ca76889dceeaed502229745c1
fimaAidarous <fma0899@gmail.com>
2025-03-31 07:34:04 -0700

    edit the createLoan

--- api/controllers/loanController.js
+++ api/controllers/loanController.js
@@ -1,28 +1,32 @@
 import Loan from "../Models/loanModel.js"; 
 import User from "../Models/userModel.js";
 
-export const createLoan = async (req,res, next) => {
-    try {
-      const { user_id, amount, loan_type,status } = req.body;
-      
+export const createLoan = async (req, res, next) => {
+  try {
+      const { user_id, amount, loan_type, status } = req.body;
+
       const user = await User.findById(user_id);
       if (!user) {
-        return next ({ status: 404, message: "User not found"});
+          return next({ status: 404, message: "User not found" });
       }
 
       const newLoan = new Loan({
-        user_id,
-        amount,
-        loan_type,
-        status,
+          user_id,
+          amount,
+          loan_type,
+          status,
       });
+      await newLoan.save();
 
-      res.status(201).json({ message: "Loan created successfully",
-        loan: newLoan});
-    } catch (error) {
-        next(error);
-    }
+      res.status(201).json({
+          message: "Loan created successfully",
+          loan: newLoan
+      });
+  } catch (error) {
+      next(error);
+  }
 };
+
 
 export const getAllLoans = async (req, res, next) => {
   try {
