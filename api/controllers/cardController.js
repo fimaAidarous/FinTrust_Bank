933ccadc51b3ec90bd8a90ce38ca9c2c1566f645
fimaAidarous <fma0899@gmail.com>
2025-04-01 00:22:52 -0700

    getAllCards

--- api/controllers/cardController.js
+++ api/controllers/cardController.js
@@ -4,7 +4,7 @@ import User from "../Models/userModel.js";
 export const createCard = async (req, res, next) => {
     try {
         const { user_id, card_number, card_type, cvv, expiry_date, status } = req.body;
-        
+
         if (!expiry_date || isNaN(Date.parse(expiry_date))) {
             return res.status(400).json({ message: "Invalid or missing expiry_date" });
         }
@@ -25,6 +25,18 @@ export const createCard = async (req, res, next) => {
 
         await newCard.save();
         res.status(201).json({ message: "Card created successfully", card: newCard });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getAllCards = async (req, res, next) => {
+    try {
+        const cards = await Card.find().populate("user_id", "name email");
+        if (cards.length === 0) {
+            return res.status(404).json({ message: "No cards found" });
+        }
+        res.status(200).json(cards);
     } catch (error) {
         next(error);
     }
--- api/routes/cardRoutes.js
+++ api/routes/cardRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {
     createCard,
+    getAllCards 
 } from "../controllers/cardController.js";
 
 const router = express.Router();
 
 router.post("/", createCard);
+router.get("/", getAllCards);
 
 export default router;
